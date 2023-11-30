43b9ea44638c0c1d4696a413496c036ee8df966f
AndreaBartoloni <andrea.bartoloni00@gmail.com>
2023-11-30 16:00:21 +0100

    commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+import card from './components/card.vue'
+
+export default{
+  components: {
+    card
+  },
+  data() {
+    return {
+      
+    }
+  },
+  methods: {
+    
+  },
+}
+</script>
+
+<template>
+
+  <card title="ciao"/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/card.vue
@@ -0,0 +1,19 @@
+<script>
+export default{
+    props: {
+        title: String
+    } 
+}
+
+
+</script>
+
+<template>
+    <h1>{{ title }}</h1>
+</template>
+
+<style scoped>
+h1{
+    color: white;
+}
+</style>
